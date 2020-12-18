9fbbd1de1798f0bd350b99a983b469a3b8d2404d
Mamata24 <nmamta240@gmail.com>
2020-12-18 12:00:25 +0530

    Box Components

---
+++ index.js
@@ -0,0 +1,32 @@
+var focusBox1 = document.getElementById("box1")
+    focusBox1.addEventListener('click', function() {
+    box1.classList.toggle('inactive');
+	box1.classList.toggle('active');
+
+})
+
+var focusBox2 = document.getElementById("box2")
+focusBox2.addEventListener('click', function() {
+    box2.classList.toggle('inactive');
+	box2.classList.toggle('active');
+})
+
+var focusBox3 = document.getElementById("box3")
+focusBox3.addEventListener('click', function() {
+    box3.classList.toggle('inactive');
+	box3.classList.toggle('active');
+})
+
+var focusBox4 = document.getElementById("box4")
+    focusBox4.addEventListener('click', function() {
+    box4.classList.toggle('inactive');
+	box4.classList.toggle('active');
+
+})
+
+
+var focusBox5 = document.getElementById("box5")
+focusBox5.addEventListener('click', function() {
+    box5.classList.toggle('inactive');
+	box5.classList.toggle('active');
+})
